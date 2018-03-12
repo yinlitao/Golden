e554b7788a999a1c8680ef94a03523a37879ae63
yinlitao <yinlitao@yahoo.com>
2018-03-12 14:02:57 -0500

    test git

--- Web/Models/ErrorViewModel.cs
+++ Web/Models/ErrorViewModel.cs
@@ -4,8 +4,8 @@ namespace Web.Models
 {
     public class ErrorViewModel
     {
-        public string RequestId { get; set; }
 
+        public string RequestId { get; set; }
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
